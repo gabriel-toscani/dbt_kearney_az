fc531efda152fc6b0e693420741b95560aab0bed
Gabriel Toscani <gabrieltoscani96@gmail.com>
2023-11-30 16:11:30 +0000

    First dbt class

---
+++ models/sources/dvdrental_sources.sql
@@ -0,0 +1,15 @@
+{{ codegen.generate_source(schema_name = 'RAW_DVDRENTAL', database_name = 'NORTHWIND', table_names = [
+"CITY"
+, "CUSTOMER"
+, "FILM_CATEGORY"
+, "INVENTORY"
+, "RENTAL"
+, "ADDRESS"
+, "CATEGORY"
+, "PAYMENT"
+, "ACTOR"
+, "COUNTRY"
+, "FILM"
+, "FILM_ACTOR"
+, "STORE"
+, "STAFF"], generate_columns = True) }}
